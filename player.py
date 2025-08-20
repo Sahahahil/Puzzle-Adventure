4a4d1608ab112b66731e2cdbca55c26ee86af732
Sahahahil <sahilduwal@gmail.com>
2025-08-20 20:34:59 +0545

    added player.py file

---
+++ player.py
@@ -0,0 +1,19 @@
+import pygame
+
+class Player:
+    def __init__(self, x, y):
+        self.image = pygame.Surface((40, 40))
+        self.image.fill((0, 255, 0))
+        self.rect = self.image.get_rect(topleft=(x, y))
+        self.speed = 5
+
+    def move(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]: self.rect.x -= self.speed
+        if keys[pygame.K_RIGHT]: self.rect.x += self.speed
+        if keys[pygame.K_UP]: self.rect.x -= self.speed
+        if keys[pygame.K_DOWN]: self.rect.x += self.speed
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+        
